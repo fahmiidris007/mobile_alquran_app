be6ef1fe48bcbb939993c8639bb5124a2fc4972e
fahmiidris <fahmiidris@apps.ipb.ac.id>
2024-03-12 22:47:34 +0700

    initial project

---
+++ android/app/src/main/kotlin/com/example/mobile_alquran_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mobile_alquran_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Container(
+          color: Colors.white,
+          child: const Center(
+            child: Text(
+              'Initial Project',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ));
+  }
+}
