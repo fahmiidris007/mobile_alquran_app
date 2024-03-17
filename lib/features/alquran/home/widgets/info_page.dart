c3c76caaca317b9c26f69459a1d4f6702cefad95
fahmiidris <fahmiidris@apps.ipb.ac.id>
2024-03-17 11:45:32 +0700

    feat: info contact page

--- lib/features/alquran/home/widgets/info_page.dart
+++ lib/features/alquran/home/widgets/info_page.dart
@@ -1,12 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_alquran_app/config/themes/AppColors.dart';
+import 'package:mobile_alquran_app/features/alquran/home/widgets/build_text.dart';
 
 class InfoPage extends StatelessWidget {
   const InfoPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Info Page"),
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            BuildText(
+              text: 'Mobile App Developer :',
+              fontSize: 24,
+              fontWeight: FontWeight.normal,
+              color: Colors.white,
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            BuildText(
+              text: 'Fahmi Idris',
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+              color: Colors.white,
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            BuildText(
+              text: '(linkedin.com/in/fahmi-idris-87ba22259)',
+              fontSize: 18,
+              fontWeight: FontWeight.normal,
+              color: Colors.white,
+            ),
+            SizedBox(
+              height: 24
+            ),
+            BuildText(
+              text: 'UI Designer :',
+              fontSize: 24,
+              fontWeight: FontWeight.normal,
+              color: Colors.white,
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            BuildText(
+              text: 'Tanvir Ahassan',
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+              color: Colors.white,
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            BuildText(
+              text: '(www.instagram.com/tanvir_ux)',
+              fontSize: 18,
+              fontWeight: FontWeight.normal,
+              color: Colors.white,
+            ),
+            SizedBox(
+                height: 24
+            ),
+            BuildText(
+              text: 'API Data Source :',
+              fontSize: 24,
+              fontWeight: FontWeight.normal,
+              color: Colors.white,
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            BuildText(
+              text: 'EQuran.id',
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+              color: Colors.white,
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            BuildText(
+              text: '(https://equran.id/apidev/v2)',
+              fontSize: 18,
+              fontWeight: FontWeight.normal,
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
